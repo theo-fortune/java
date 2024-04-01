915d472490d59201c2651725832c359bde9b24eb
Theo Fortune <theofortunefunction@gmail.com>
2024-04-01 18:52:51 +0100

    Data types in Java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
 class Hello {
   public static void main(String a[]) 
   {
-    int num = 3;
-    System.out.println("Hello World");
+    int num1 = 3;
+    byte by = 127;
+    short sh = 558;
+    long lg = 21031l;
+    float f = 5.3f;
+    double db = 9.3;
+    char c = 'k';
+    boolean b = true;
   }
 }
