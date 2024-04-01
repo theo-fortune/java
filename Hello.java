75bbc2d1dec265cc2cd8d09f214c9945de1b5f45
Theo Fortune <theofortunefunction@gmail.com>
2024-04-01 19:02:28 +0100

    literals

--- Hello.java
+++ Hello.java
@@ -1,13 +1,8 @@
 class Hello {
   public static void main(String a[]) 
   {
-    int num1 = 3;
-    byte by = 127;
-    short sh = 558;
-    long lg = 21031l;
-    float f = 5.3f;
-    double db = 9.3;
-    char c = 'k';
-    boolean b = true;
+    char c = '1';
+    c++;
+    System.out.println(c);
   }
 }
