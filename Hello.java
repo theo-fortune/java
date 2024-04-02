3c4fd693313e1dd3b1783c7682abbe4e6c512e4d
Theo Fortune <theofortunefunction@gmail.com>
2024-04-02 04:28:17 +0100

    relational operators

--- Hello.java
+++ Hello.java
@@ -1,12 +1,9 @@
 class Hello {
   public static void main(String args[]) 
   {
-    int num = 7;
-    //* post increment
-    int result = num++; 
-    //* pre increment
-    // int result = ++num; 
-
+    int x = 6;
+    int y = 5;
+    boolean result = x == y;
     System.out.println(result);
   }
 }
