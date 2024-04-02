7f24e88ee261b95784461c08145b5f30706d1bf6
Theo Fortune <theofortunefunction@gmail.com>
2024-04-02 21:45:40 +0100

    if else

--- Hello.java
+++ Hello.java
@@ -1,10 +1,11 @@
 class Hello {
   public static void main(String args[]) {
     int x = 7;
-    int y = 5;
-    int a = 5;
-    int b = 9;
-    boolean result = a > b;
-    System.out.println(!result);
+    int y = 7;
+    if (x > y) {
+      System.out.println(x);
+    } else {
+      System.out.println(y);
+    }
   }
 }
