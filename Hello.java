29d95437c338565bf591a6a393be361a3f337058
Theo Fortune <theofortunefunction@gmail.com>
2024-04-02 04:15:08 +0100

    operators

--- Hello.java
+++ Hello.java
@@ -1,11 +1,12 @@
 class Hello {
   public static void main(String args[]) 
   {
-    int a = 257;
-    byte k = (byte) a;
-    float fl = 5.9f;
-    int t = (int) fl;
-    System.out.println(k);
-    System.out.println(t);
+    int num = 7;
+    //* post increment
+    int result = num++; 
+    //* pre increment
+    // int result = ++num; 
+
+    System.out.println(result);
   }
 }
