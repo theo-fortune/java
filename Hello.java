c4ae4cc48071f099732afa46524a616acd59f89a
Theo Fortune <theofortunefunction@gmail.com>
2024-04-03 18:55:36 +0100

    switch statements

--- Hello.java
+++ Hello.java
@@ -1,15 +1,28 @@
 class Hello {
   public static void main(String args[]) {
-    int x = 4;
-    int result = 0;
-    if (x % 2 == 0) {
-      result = 10;
-      System.out.println(result);
-    } else {
-      result = 20;
-      System.out.println(result);
+    int days = 5;
+    switch (days) {
+      case 1:
+        System.out.println("Monday");
+        break;
+      case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;
     }
-    int points = 0;
-    points = x % 2 == 0 ? 10 : 20;
   }
 }
