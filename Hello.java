52cab81fe4c325e4cad758f0a73da606bab14ed8
Theo Fortune <theofortunefunction@gmail.com>
2024-04-16 12:04:13 +0100

    while loop

--- Hello.java
+++ Hello.java
@@ -1,15 +1,9 @@
 class Hello {
   public static void main(String args[]) {
     int i = 1;
-    while (i <= 4) {
+    do {
       System.out.println("Hi " + i);
-      int j = 1;
-      while (j<=3) {
-        System.out.println("Hello " + j);
-        j++;
-      }
       i++;
-    }
-    System.out.println("Bye " + i);
+    } while (i <= 4);
   }
 };
