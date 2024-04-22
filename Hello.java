43d89d90fcfc7be6b17334d5342354f92a76ce6f
Theo Fortune <theofortunefunction@gmail.com>
2024-04-22 11:57:39 +0100

    for loop

--- Hello.java
+++ Hello.java
@@ -1,10 +1,8 @@
 class Hello {
   public static void main(String args[]) {
-    int i = 5;
-    do {
+    for (int i = 1; i <= 4; i++) {
       System.out.println("Hi " + i);
-      i++;
-    } while (i <= 4);
+    }
   }
   // * If you want to get the output once even if the condition is false, use a doWhile loop
 };
