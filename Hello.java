6a17bc5bb1328f2cd1274ab99385f850c067ebc9
Theo Fortune <theofortunefunction@gmail.com>
2024-05-13 12:31:26 +0100

    methods

--- Hello.java
+++ Hello.java
@@ -1,17 +1,23 @@
-class Calculator {
-  public int add(int n1, int n2) {
-    int r = n1 + n2;
-    return  r;
+class Computer {
+  public void playMusic() {
+    System.out.println("Music Playing...");
+  }
+
+  public String getMeAPen(int cost) {
+    if (cost >=10) {
+      return "Pen";
+    } else {
+      return "Nothing";
+    }
   }
 }
 
 public class Hello {
   public static void main(String args[]) {
-    int num1 = 4;
-    int num2 = 5;
-    Calculator calc = new Calculator();
-    int result = calc.add(num1, num2);
-    System.out.println(result);
+    Computer obj = new Computer();
+    obj.playMusic();
+    String str = obj.getMeAPen(2);
+    System.out.println(str);
   }
 }
 
